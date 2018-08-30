df0a4cad879b645a260b1c53bfece7e73e549976
Jessica Howard <jessicahoward1031@gmail.com>
2018-08-30 10:24:37 -0400

    models created for aadding tables to database

---
+++ models/education.js
@@ -0,0 +1,13 @@
+//model for education table
+module.exports = function(sequelize, DataTypes) {
+  var Education = sequelize.define("Education", {
+    school: {type: DataTypes.STRING,
+                allowNull: false},
+    degree: {type: DataTypes.STRING},           
+    startDate: {type: DataTypes.DATEONLY,
+        allowNull: false},
+    endDate: DataTypes.DATEONLY
+    
+  });
+  return Education;
+};
