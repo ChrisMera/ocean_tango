cba03c5a1724a36728a083c021f92593802423e7
Chupi <cmera001@fiu.edu>
2018-08-31 15:16:36 -0400

    test

---
+++ public/js/add-edu.js
@@ -0,0 +1,48 @@
+// PASSPORT addRef
+// =============================================
+$(document).ready(function() {
+  // Getting references to our form and input
+  var addEduForm = $("form.add-edu");
+  var refNameInput = $("input#ref-name-input");
+  var refPhoneInput = $("input#ref-phone-input");
+  var refRelInput = $("input#ref-rel-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  addEduForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      name: refNameInput.val().trim(),
+      phone: refPhoneInput.val().trim(),
+      relationship: refRelInput.val().trim()
+    };
+    console.log(userData);
+
+    if (!userData.name || !userData.phone) {
+      return;
+    }
+    // If we have an project bame and asscSkill, run the addJob function
+    addRef(userData.name, userData.phone, userData.relationship);
+    refNameInput.val("");
+    refPhoneInput.val("");
+    refRelInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function addRef(name, phone, relationship) {
+    $.post("/api/ref", {
+      name: name,
+      phone: phone,
+      relationship: relationship
+    }).then(function() {
+      // window.location.replace(data);
+      // If there's an error, handle it by throwing up a boostrap alert
+    });
+    //   .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
