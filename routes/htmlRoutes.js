ba4478eb679bf0d121b46f98119a243e0372591f
Jessica Howard <jessicahoward1031@gmail.com>
2018-08-30 20:59:36 -0400

    cleanup routes, add handlebars for objective

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,42 +1,3 @@
-// var db = require("../models");
-
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    res.render("index");
-  });
-
-  // Load login page
-  app.get("/login", function(req, res) {
-
-    res.render("login");
-  });
-
-  //===== Load new-project page
-  app.get("/new-job", function(req, res) {
-    res.render("new-job");
-  });
-
-  //===== Load dashboard page
-  app.get("/dashboard", function(req, res) {
-    res.render("dashboard");
-
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("login", {
-        // example: dbExample
-      });
-    });
-
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
-
 // PASSPORT ROUTES
 // =============================================
 // Requiring path to so we can use relative routes to our HTML files
@@ -46,12 +7,25 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
+  //===== Load new-project page
+  app.get("/new-job", function(req, res) {
+    res.render("new-job");
+  });
+
+  //===== Load dashboard page
+  app.get("/dashboard", function(req, res) {
+    res.render("dashboard");
+  });
+
+  // Render 404 page for any unmatched routes
+
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
+    console.log("HERE");
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("index");
   });
 
   app.get("/login", function(req, res) {
@@ -59,13 +33,29 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
+  });
+
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.render("signup");
+  });
+
+  app.get("/objective", function(req, res) {
+    res.render("objective");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+    
+  app.get("*", function(req, res) {
+    res.render("404");
   });
 };
 // PASSPORT ROUTES END
