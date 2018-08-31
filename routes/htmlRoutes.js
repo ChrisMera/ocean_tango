e45e62d64bcf25823edea6b372b6837692463b90
erikadelgado455 <erikadelgado455@yahoo.com>
2018-08-31 14:17:25 -0400

    fixed more linting issues

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -28,64 +28,60 @@ module.exports = function(app) {
     res.render("signup");
   });
 
-
-  
-app.get("/searchjob", function(req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/members");
-  }
-  res.render("searchjob");
-});
-
-  //PULLS BACK THE VIEW FOR SIGNUP.HANDLEBARS PAGE
-  app.get("/signup", function(req, res) {
-
-  app.get("/login", function(req, res) {
-
+  app.get("/searchjob", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/dashboard");
+      res.redirect("/members");
     }
-    res.render("login");
-  });
-  //===== Load dashboard page
-  app.get("/dashboard", function(req, res) {
-    res.render("dashboard");
-  });
-  app.get("/add-job", function(req, res) {
-    res.render("add-job");
-  });
-  app.get("/add-ref", function(req, res) {
-    res.render("add-ref");
-  });
-  app.get("/add-edu", function(req, res) {
-    res.render("add-edu");
-  });
-  // Render 404 page for any unmatched routes
-  app.get("/objective", function(req, res) {
-    res.render("objective");
+    res.render("searchjob");
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.redirect("/dashboard");
-  });
+  //PULLS BACK THE VIEW FOR SIGNUP.HANDLEBARS PAGE
+  app.get(
+    "/signup",
+    function() {
+      app.get("/login", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+          res.redirect("/dashboard");
+        }
+        res.render("login");
+      });
+      //===== Load dashboard page
+      app.get("/dashboard", function(req, res) {
+        res.render("dashboard");
+      });
+      app.get("/add-job", function(req, res) {
+        res.render("add-job");
+      });
+      app.get("/add-ref", function(req, res) {
+        res.render("add-ref");
+      });
+      app.get("/add-edu", function(req, res) {
+        res.render("add-edu");
+      });
+      // Render 404 page for any unmatched routes
+      app.get("/objective", function(req, res) {
+        res.render("objective");
+      });
 
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-});
+      // Here we've add our isAuthenticated middleware to this route.
+      // If a user who is not logged in tries to access this route they will be redirected to the signup page
+      app.get("/members", isAuthenticated, function(req, res) {
+        res.redirect("/dashboard");
+      });
 
+      app.get("*", function(req, res) {
+        res.render("404");
+      });
+    }
+    // PASSPORT ROUTES
+    // =============================================
 
+    // PASSPORT ROUTES END
+    // =============================================
 
-// PASSPORT ROUTES
-// =============================================
-
-// PASSPORT ROUTES END
-// =============================================
-
-// ROUTES END
-// ======================================================
-
+    // ROUTES END
+    // =====================================================/
+  );
+};
