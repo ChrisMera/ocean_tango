9d49b1f3dcf69fa0acc2f72933c268b79d495d66
Bernadette Januska <bmjanuska@gmail.com>
2018-08-31 13:27:50 -0400

    Add jobs routes updated

--- public/js/add-job.js
+++ public/js/add-job.js
@@ -3,48 +3,48 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var addJobForm = $("form.add-job");
-  var typeInput = $("select#new-type-select");
   var jobName = $("input#job-name-input");
   var roleInput = $("input#role-input");
+  var descriptionInput = $("input#description-input");
   var startDateInput = $("input#start-input");
   var endDateInput = $("input#end-input");
-  var descriptionInput = $("input#description-input");
   var skillsInput = $("input#skills-input");
+  var typeInput = $("select#new-type-select");
 
   // When the signup button is clicked, we validate the email and password are not blank
   addJobForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      expType: typeInput.val(),
       name: jobName.val().trim(),
       role: roleInput.val().trim(),
+      description: descriptionInput.val().trim(),
       startDate: startDateInput.val().trim(),
       endDate: endDateInput.val().trim(),
-      description: descriptionInput.val().trim(),
-      asscSkills: skillsInput.val().trim()
+      asscSkills: skillsInput.val().trim(),
+      expType: typeInput.val()
     };
-    console.log(userData);
+    console.log("THIS IS A USER: " + userData);
 
     if (!userData.name || !userData.asscSkills) {
       return;
     }
     // If we have an project bame and asscSkill, run the addJob function
     addJob(
-      userData.expType,
       userData.name,
       userData.role,
+      userData.description,
       userData.startDate,
       userData.endDate,
-      userData.description,
-      userData.asscSkills
+      userData.asscSkills,
+      userData.expType
     );
-    typeInput.val("");
     jobName.val("");
     roleInput.val("");
+    descriptionInput.val("");
     startDateInput.val("");
     endDateInput.val("");
-    descriptionInput.val("");
     skillsInput.val("");
+    typeInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
@@ -66,16 +66,15 @@ $(document).ready(function() {
       endDate: endDate,
       asscSkills: asscSkills,
       expType: expType
-    })
-      .then(function(data) {
-        window.location.replace(data);
-        // If there's an error, handle it by throwing up a boostrap alert
-      })
-    //   .catch(handleLoginErr);
+    }).then(function(data) {
+      window.location.replace(data);
+      // If there's an error, handle it by throwing up a boostrap alert
+    });
+    //  ; .catch(handleLoginErr);
   }
 
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
-  }
+  // function handleLoginErr(err) {
+  //   $("#alert .msg").text(err.responseJSON);
+  //   $("#alert").fadeIn(500);
+  // }
 });
