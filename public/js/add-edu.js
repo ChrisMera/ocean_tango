2b250a67a1b0ce453435b26e9cff10d7e88d55f9
Chupi <cmera001@fiu.edu>
2018-08-31 15:39:48 -0400

    edu page captures values and routes to db successfully

--- public/js/add-edu.js
+++ public/js/add-edu.js
@@ -1,39 +1,48 @@
-// PASSPORT addRef
+// PASSPORT addEdu
 // =============================================
 $(document).ready(function() {
   // Getting references to our form and input
   var addEduForm = $("form.add-edu");
-  var refNameInput = $("input#ref-name-input");
-  var refPhoneInput = $("input#ref-phone-input");
-  var refRelInput = $("input#ref-rel-input");
+  var eduNameInput = $("input#school-name-input");
+  var degreeInput = $("input#degree-input");
+  var startDateInput = $("input#edu-start-input");
+  var endDateInput = $("input#edu-end-input");
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // When the add button is clicked, we validate the name
   addEduForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      name: refNameInput.val().trim(),
-      phone: refPhoneInput.val().trim(),
-      relationship: refRelInput.val().trim()
+      school: eduNameInput.val().trim(),
+      degree: degreeInput.val().trim(),
+      startDate: startDateInput.val().trim(),
+      endDate: endDateInput.val().trim()
     };
     console.log(userData);
 
-    if (!userData.name || !userData.phone) {
+    if (!userData.school) {
       return;
     }
-    // If we have an project bame and asscSkill, run the addJob function
-    addRef(userData.name, userData.phone, userData.relationship);
-    refNameInput.val("");
-    refPhoneInput.val("");
-    refRelInput.val("");
+    // If we have a school name, run the addEdu function
+    addEdu(
+      userData.school,
+      userData.degree,
+      userData.startDate,
+      userData.endDate
+    );
+    eduNameInput.val("");
+    degreeInput.val("");
+    startDateInput.val("");
+    endDateInput.val("");
   });
 
-  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Does a post to the edu route.
   // Otherwise we log any errors
-  function addRef(name, phone, relationship) {
-    $.post("/api/ref", {
-      name: name,
-      phone: phone,
-      relationship: relationship
+  function addEdu(school, degree, startDate, endDate) {
+    $.post("/api/edu", {
+      school: school,
+      degree: degree,
+      startDate: startDate,
+      endDate: endDate
     }).then(function() {
       // window.location.replace(data);
       // If there's an error, handle it by throwing up a boostrap alert
